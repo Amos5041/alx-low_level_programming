768a7322d08a1db1eecaeacd248ee17c497258e5
Amouseh15 <Amo5041>
2023-03-16 20:06:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A programm that prints a line using the peintf function
+ * Return: 0 (success)
+ **/
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art\n")
+	return (0);
+}
